8e28c953d51207928c61ff3d6ef4859241f5d977
Javohirbek <safarovjavohirbek3@gmail.com>
2023-09-14 12:15:18 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import math
+
+a = float(input("Koefitsentni kiriting a: "))
+b = float(input("Koefitsentni kiriting b: "))
+c = float(input("Koefitsentni kiriting c: "))
+
+discriminant = b**2 - 4*a*c
+
+if discriminant > 0:
+    x1 = (-b + math.sqrt(discriminant)) / (2 * a)
+    x2 = (-b - math.sqrt(discriminant)) / (2 * a)
+elif discriminant == 0:
+    x = -b / (2 * a)
+    print(f"The root is {x}")
+else:
+    realPart = -b / (2*a)
+    imaginaryPart = math.sqrt(-discriminant) / (2*a)
+    print(f"Complex ildizlar: {realPart} + {imaginaryPart}i va {realPart} - {imaginaryPart}i")
