3bde499e39f9f0bf689aa51906b39c0ec079d23b
Javohirbek <safarovjavohirbek3@gmail.com>
2023-09-15 10:19:44 +0500

    Initial commit

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ discriminant = b**2 - 4*a*c
 if discriminant > 0:
     x1 = (-b + math.sqrt(discriminant)) / (2 * a)
     x2 = (-b - math.sqrt(discriminant)) / (2 * a)
+    print(f"X1 = {x1}, X2 = {x2}")
 elif discriminant == 0:
     x = -b / (2 * a)
     print(f"yagona ildiz: {x}")
